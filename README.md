226bf4c4622d5d34bbb3c5a25230e642ca9ee5ee
kimdevhwan <124761753+kimdevhwan@users.noreply.github.com>
2023-04-05 18:26:33 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Typescript-Study
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-7kayid)
---
+++ aboutType.ts
@@ -0,0 +1,71 @@
+//타입 추론
+let a = 1;
+let b = 'ahahah';
+
+//타입 명시
+let c: String = 'yeah';
+let d: number[] = [1, 2, 3];
+
+//타입 alias
+type programmer = {
+  name: String;
+  skills: String[];
+  age?: number; //? is optional
+};
+
+let f: programmer = {
+  name: '김동환',
+  skills: ['java', 'mysql'],
+};
+
+type name = string;
+type playerNumber = number;
+type player = {
+  name: name;
+  no: playerNumber;
+  age?: number;
+};
+
+//리턴 타입 정의
+function playerMaker(name: name, no: playerNumber): player {
+  return {
+    name,
+    no,
+  };
+}
+
+const kimdevhwan = playerMaker('kimdevhwan', 4);
+kimdevhwan.age = 23;
+
+//화살표 함수
+const playerMaker2 = (name: string, no: number): player => ({ name, no });
+const son = playerMaker2('sonheungmin', 7);
+son.age = 32;
+
+console.log('Hell kimdevhwan! welcome to typeworld.');
+
+//readonly
+const names: readonly string[] = ['1', '2', '3'];
+names.push('ahahah'); //Immutable 불변성, 에러 붐
+
+//tuple 배열 타입을 특수하게 정의, 순서에 따른 타입을 맞춰줘야한다.
+let errdreamers: [string, number, boolean, string?] = [
+  'kimdevhwan',
+  230314,
+  'true',
+]; //에러 붐
+errdreamers[2] = false;
+errdreamers[3] = "it's optional";
+
+const realdreamers: [number, string, boolean?] = [230314, 'kimdevhwan'];
+realdreamers[0] = '1234'; //에러 붐
+realdreamers[2] = true;
+
+// any를 쓸거면 왜 타입스크립트를 쓰나?
+// 그런 상황이 있을거라고? 글쎄.. 난 잘 모르겠다.
+let badboy: any = "it's too bad";
+let badgirl: any = 1818;
+console.log(badboy + badgirl); //WTF
+
+//unknown
+let gg: unknown;
